6adef8344c0bd3d41c45b9a9dac4e53df454ba13
Darya <dasha-439@yandex.ru>
2025-02-09 17:50:26 +0300

    Создание функции сортировки банковских операций по дате - sort_by_date, оформление main.py файла для вызова обеих функций.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from src.processing import filter_by_state
+from src.processing import filter_by_state, sort_by_date
 
 choose_state = "CANCELED"
+sort_date = False
 log_operation = [
     {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
     {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
@@ -9,3 +10,4 @@ log_operation = [
 ]
 if __name__ == "__main__":
     print(filter_by_state(log_operation, choose_state))
+    print(sort_by_date(log_operation, sort_date))
--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,13 @@
 def filter_by_state(log_operation: list[dict], choose_state: str = "EXECUTED") -> list[dict]:
-    """Создание функции фильтрации банковских операций по значению ключа state"""
+    """Функция фильтрации банковских операций по значению ключа state"""
     selected_operation = []
     for operation in log_operation:
         if operation.get("state") == choose_state:
             selected_operation.append(operation)
     return selected_operation
+
+
+def sort_by_date(log_operation: list[dict], sort_date: bool = True) -> list[dict]:
+    """Функция сортировки банковских операций по дате"""
+    sorted_operation = sorted(log_operation, key=lambda x: x["date"], reverse=sort_date)
+    return sorted_operation
