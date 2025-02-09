22c852fddd74ce5bb6967be8428b98d272321fd9
Darya <dasha-439@yandex.ru>
2025-02-09 18:38:09 +0300

    homework_10_1 готова

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ log_operation = [
     {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
     {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
 ]
+
 if __name__ == "__main__":
     print(filter_by_state(log_operation, choose_state))
     print(sort_by_date(log_operation, sort_date))
--- src/processing.py
+++ src/processing.py
@@ -8,6 +8,6 @@ def filter_by_state(log_operation: list[dict], choose_state: str = "EXECUTED") -
 
 
 def sort_by_date(log_operation: list[dict], sort_date: bool = True) -> list[dict]:
-    """Функция сортировки банковских операций по дате"""
+    """Функция сортировки банковских операций по дате создания"""
     sorted_operation = sorted(log_operation, key=lambda x: x["date"], reverse=sort_date)
     return sorted_operation
