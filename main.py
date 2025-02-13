3daa2f7e12c0a64409aa64b910e3a4384a5f4584
Darya <dasha-439@yandex.ru>
2025-02-13 21:59:09 +0300

    homework_10_1 готова

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from src.processing import filter_by_state, sort_by_date
+from src import widget
 
 choose_state = "CANCELED"
 sort_date = False
@@ -12,3 +13,17 @@ log_operation = [
 if __name__ == "__main__":
     print(filter_by_state(log_operation, choose_state))
     print(sort_by_date(log_operation, sort_date))
+
+
+bank_info = input("Введите информацию о банковском счете/карте: ")
+date_user = input("Введите дату ")
+
+if len(bank_info) <= 20:
+    print("Неверный формат счета, укажите тип счета и проверьте количество цифр в номере")
+else:
+    print(f"Маска: {widget.mask_account_card(bank_info)}")
+
+if len(date_user) != 26:
+    print("Неверный формат даты")
+else:
+    print(f"Новый формат даты: {widget.get_date(date_user)}")
