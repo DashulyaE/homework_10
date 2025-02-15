35f77bce06eea4744cdbf80468bb4516d63ef276
Darya <dasha-439@yandex.ru>
2025-02-15 23:33:39 +0300

    Корректировка кода для анализа типа корректности введенного счета

--- src/widget.py
+++ src/widget.py
@@ -1,28 +1,26 @@
 from datetime import datetime
-
 from src import masks
 
 
 def mask_account_card(bank_info: str) -> str:
     """Функция, которая маскирует банковский счет, либо номер банковской карты"""
-    naim_card = ""
     number_card = ""
-    mask_card = ""
 
-    for i in bank_info:
-        if i.isalpha():
-            naim_card += i
-        if i == " ":
-            naim_card += " "
+    for i in reversed(bank_info):
         if i.isdigit():
             number_card += i
+        elif i.isalpha() or i == " ":
+            break
 
+    name_end = len(bank_info) - len(number_card)
+    number_card = number_card[::-1]
+    name_card = bank_info[:name_end]
 
     if len(number_card) == 16:
-        mask_card = naim_card + " " + masks.get_mask_card_number(number_card)
+        mask_card = name_card + " " + masks.get_mask_card_number(number_card)
         return mask_card
     elif len(number_card) == 20:
-        mask_card = naim_card + " " + masks.get_mask_account(number_card)
+        mask_card = name_card + " " + masks.get_mask_account(number_card)
         return mask_card
     else:
         return "Проверьте правильность счета(карта - 16 цифр, счет - 20 цифр)"
