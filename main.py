21490d70821ca723b9f1a6020143d6431a3f9217
Darya <dasha-439@yandex.ru>
2025-02-23 14:35:33 +0300

    Реализация тестирования функции filter_by_currency

--- main.py
+++ main.py
@@ -12,105 +12,75 @@ log_operation = [
 ]
 
 transactions = [
-        {
-            "id": 939719570,
-            "state": "EXECUTED",
-            "date": "2018-06-30T02:08:58.425572",
-            "operationAmount": {
-                "amount": "9824.07",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод организации",
-            "from": "Счет 75106830613657916952",
-            "to": "Счет 11776614605963066702"
-        },
-        {
-            "id": 142264268,
-            "state": "EXECUTED",
-            "date": "2019-04-04T23:20:05.206878",
-            "operationAmount": {
-                "amount": "79114.93",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод со счета на счет",
-            "from": "Счет 19708645243227258542",
-            "to": "Счет 75651667383060284188"
-        },
-        {
-            "id": 873106923,
-            "state": "EXECUTED",
-            "date": "2019-03-23T01:09:46.296404",
-            "operationAmount": {
-                "amount": "43318.34",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
-            },
-            "description": "Перевод со счета на счет",
-            "from": "Счет 44812258784861134719",
-            "to": "Счет 74489636417521191160"
-        },
-        {
-            "id": 895315941,
-            "state": "EXECUTED",
-            "date": "2018-08-19T04:27:37.904916",
-            "operationAmount": {
-                "amount": "56883.54",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод с карты на карту",
-            "from": "Visa Classic 6831982476737658",
-            "to": "Visa Platinum 8990922113665229"
-        },
-        {
-            "id": 594226727,
-            "state": "CANCELED",
-            "date": "2018-09-12T21:27:25.241689",
-            "operationAmount": {
-                "amount": "67314.70",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
-            },
-            "description": "Перевод организации",
-            "from": "Visa Platinum 1246377376343588",
-            "to": "Счет 14211924144426031657"
-        }
+    {
+        "id": 939719570,
+        "state": "EXECUTED",
+        "date": "2018-06-30T02:08:58.425572",
+        "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод организации",
+        "from": "Счет 75106830613657916952",
+        "to": "Счет 11776614605963066702",
+    },
+    {
+        "id": 142264268,
+        "state": "EXECUTED",
+        "date": "2019-04-04T23:20:05.206878",
+        "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод со счета на счет",
+        "from": "Счет 19708645243227258542",
+        "to": "Счет 75651667383060284188",
+    },
+    {
+        "id": 873106923,
+        "state": "EXECUTED",
+        "date": "2019-03-23T01:09:46.296404",
+        "operationAmount": {"amount": "43318.34", "currency": {"name": "руб.", "code": "RUB"}},
+        "description": "Перевод со счета на счет",
+        "from": "Счет 44812258784861134719",
+        "to": "Счет 74489636417521191160",
+    },
+    {
+        "id": 895315941,
+        "state": "EXECUTED",
+        "date": "2018-08-19T04:27:37.904916",
+        "operationAmount": {"amount": "56883.54", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод с карты на карту",
+        "from": "Visa Classic 6831982476737658",
+        "to": "Visa Platinum 8990922113665229",
+    },
+    {
+        "id": 594226727,
+        "state": "CANCELED",
+        "date": "2018-09-12T21:27:25.241689",
+        "operationAmount": {"amount": "67314.70", "currency": {"name": "руб.", "code": "RUB"}},
+        "description": "Перевод организации",
+        "from": "Visa Platinum 1246377376343588",
+        "to": "Счет 14211924144426031657",
+    },
 ]
 
 if __name__ == "__main__":
-    #print(filter_by_state(log_operation, choose_state))
-    #print(sort_by_date(log_operation, sort_date))
-    #bank_info = input("Введите информацию о банковском счете/карте: ")
-    #date_user = input("Введите дату ")
-    #currency_user = (input("Введите валюту (USD, RUB): ")).upper()
-    #print(f"Маска: {widget.mask_account_card(bank_info)}")
-    #print(f"Новый формат даты: {widget.get_date(date_user)}")
+    print(filter_by_state(log_operation, choose_state))
+    print(sort_by_date(log_operation, sort_date))
+    bank_info = input("Введите информацию о банковском счете/карте: ")
+    date_user = input("Введите дату ")
+    # currency_user = (input("Введите валюту (USD, RUB): ")).upper()
+    print(f"Маска: {widget.mask_account_card(bank_info)}")
+    print(f"Новый формат даты: {widget.get_date(date_user)}")
 
-    #if currency_user == "USD" or currency_user == "RUB":
+    # if currency_user == "USD" or currency_user == "RUB":
     currency_user = "USD"
     usd_transactions = generators.filter_by_currency(transactions, currency_user)
-    for _ in range(5):
+    for _ in range(2):
         print(next(usd_transactions))
-    #elif currency_user == '':
-        #print('Вы не выбрали тип валюты')
-    #else:
-        #print('Выберите верный тип валюты')
+    # elif currency_user == '':
+    # print('Вы не выбрали тип валюты')
+    # else:
+    # print('Выберите верный тип валюты')
 
-    #descriptions = generators.transaction_descriptions(transactions)
-    #for _ in range(6):
-        #print(next(descriptions))
+    descriptions = generators.transaction_descriptions(transactions)
+    for _ in range(4):
+        print(next(descriptions))
 
-    #for card_number in generators.card_number_generator(1, 5):
-        #print(card_number)
+    for card_number in generators.card_number_generator(2, 4):
+        print(card_number)
--- src/generators.py
+++ src/generators.py
@@ -2,6 +2,8 @@ from black import Iterator
 
 
 def filter_by_currency(transactions: list[dict], currency: str = "USD") -> Iterator[dict]:
+    """Функция, которая возвращает итератор, который поочередно выдает транзакции,
+    где валюта операции соответстует заданной"""
     currency = currency.upper()
     if currency in ["USD", "RUB"]:
         for item in transactions:
@@ -14,16 +16,26 @@ def filter_by_currency(transactions: list[dict], currency: str = "USD") -> Itera
 
 
 def transaction_descriptions(transactions: list[dict]) -> Iterator[str]:
+    """Функция, которая возвращает описание каждой транзакции по очереди"""
     for item in transactions:
         yield item["description"]
 
 
 def card_number_generator(start: int, stop: int) -> Iterator[str]:
+    """Функция, которая генерирует номера банковских карт в заданных диапазоне и формате"""
     if start >= 1 and start <= stop and stop <= 9999999999999999:
-        card_numbers='0000000000000000'
+        card_numbers = "0000000000000000"
         for card_number in range(start, stop + 1):
-            card_number_gen = card_numbers[:-len(str(card_number))]+str(card_number)
-            card_number_form = card_number_gen[:4] + " " + card_number_gen[4:8] + " " + card_number_gen[8:12] + " " + card_number_gen[12:]
+            card_number_gen = card_numbers[: -len(str(card_number))] + str(card_number)
+            card_number_form = (
+                card_number_gen[:4]
+                + " "
+                + card_number_gen[4:8]
+                + " "
+                + card_number_gen[8:12]
+                + " "
+                + card_number_gen[12:]
+            )
             yield card_number_form
     else:
         raise ValueError("Номер карты не в заданном диапазоне")
