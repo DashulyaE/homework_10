473e51a33611228c64ebec492e723fd236774ede
Darya <dasha-439@yandex.ru>
2025-03-14 14:38:21 +0300

    Реализовано тестирование функций read_transactions_csv и read_transactions_excel

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import os
 from config import DATA_DIR
-from src.transactions import read_transactions_csv
+from src.transactions import read_transactions_csv, read_transactions_excel
 
 
-if __name__ == '__main__':
-    operations_path = os.path.join(DATA_DIR, 'transactions.csv')
+if __name__ == "__main__":
+    operations_path = os.path.join(DATA_DIR, "transactions.csv")
+    operations_path_ex = os.path.join(DATA_DIR, "transactions_excel.xlsx")
     print(read_transactions_csv(operations_path))
-    # print(read_transactions_json(operations_path))
-    # print(get_mask_card_number('1596837868705199'))
-    #print(get_mask_account('646473678894779589'))
+    print(read_transactions_excel(operations_path_ex))
